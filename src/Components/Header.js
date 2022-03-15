b5d554c24cf4e8a21c84073576bdbdbc394021aa
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-15 22:45:44 +0300

    fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,13 @@ import '../styles/animate.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function Header() {
-    const tahiana = <button class='btn btn-outline-primary animate__animated animate__bounceInLeft'>Tahiana😎</button>
+    const tahiana = <button class='btn btn-outline-light animate__animated animate__bounceInLeft'>Tahiana😎</button>
     const tsanta = <button class='btn btn-outline-secondary animate__animated animate__fadeInDown'>Tsanta😱</button>
-    const cedric = <button class='btn btn-outline-success animate__animated animate__bounceInRight'>Cedric🤪</button>
-    const ligne_bas = <hr className='ligne-bas' class='animate__animated animate__rotateIn'/>
+    const cedric = <button class='btn btn-outline-danger animate__animated animate__bounceInRight'>Cedric🤪</button>
+    const ligne_haut = <hr class='animate__animated animate__rotateOut'/>
     return (
         <div>
-            <div className='header'>{tahiana}&nbsp;{tsanta}&nbsp;{cedric}</div>
-            {ligne_bas}
+            <div className='header'>{ligne_haut}{tahiana}&nbsp;&nbsp;&nbsp;{tsanta}&nbsp;&nbsp;&nbsp;{cedric}{ligne_haut}</div>
         </div>
     )
 }
