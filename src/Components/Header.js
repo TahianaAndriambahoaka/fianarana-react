3328d4987c2aeafe615ff127dd5567b06ffd4abc
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-15 22:26:09 +0300

    fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,8 +6,12 @@ function Header() {
     const tahiana = <button class='btn btn-outline-primary animate__animated animate__bounceInLeft'>Tahiana😎</button>
     const tsanta = <button class='btn btn-outline-secondary animate__animated animate__fadeInDown'>Tsanta😱</button>
     const cedric = <button class='btn btn-outline-success animate__animated animate__bounceInRight'>Cedric🤪</button>
+    const ligne_bas = <hr className='ligne-bas' class='animate__animated animate__rotateIn'/>
     return (
-        <div className='header'>{tahiana}&nbsp;{tsanta}&nbsp;{cedric}</div>
+        <div>
+            <div className='header'>{tahiana}&nbsp;{tsanta}&nbsp;{cedric}</div>
+            {ligne_bas}
+        </div>
     )
 }
 
