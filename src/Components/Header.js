bee44e2c486372b7b2ee69c7caf30cc0a6482fcd
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-17 18:47:21 +0300

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,7 @@
 import '../styles/Header.css';
 import '../styles/animate.min.css';
+import ImageHome from '../images/home.png';
+import ImageCommunit from '../images/COMMUN-IT.png';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function handleClick(args) {
@@ -16,10 +18,14 @@ function Header({auteur, updateAuteur}) {
     const tahiana = <button className='btn btn-outline-light animate__animated animate__bounceInLeft' onClick={() => updateAuteur('Tahiana')}>Tahiana😎</button>
     const tsanta = <button className='btn btn-outline-secondary animate__animated animate__fadeInDown' onClick={() => updateAuteur('Tsanta')}>Tsanta😱</button>
     const cedric = <button className='btn btn-outline-danger animate__animated animate__bounceInRight' onClick={() => updateAuteur('Cedric')}>Cedric🤪</button>
+    const home = <img src={ImageHome} alt="Home" height="50px" className='animate__animated animate__flash' onClick={() => updateAuteur(null)}/>
+    const comunit = <img src={ImageCommunit} alt="Home" height="50px" className='animate__animated animate__flash' onClick={() => updateAuteur(null)}/>
     const ligne = <hr className='animate__animated animate__rotateOut'/>
     return (
         <div>
-            <div className='header'>{ligne}{tahiana}&nbsp;&nbsp;&nbsp;{tsanta}&nbsp;&nbsp;&nbsp;{cedric}{ligne}</div>
+            <div className='header'>
+                {ligne}{home}&nbsp;&nbsp;&nbsp;{tahiana}&nbsp;&nbsp;&nbsp;{tsanta}&nbsp;&nbsp;&nbsp;{cedric}&nbsp;&nbsp;&nbsp;{comunit}{ligne}
+            </div>
         </div>
     )
 }
