b2ef7529c0ce7f2a8dde2e1a193ef0cbdd8fa67f
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-17 17:01:25 +0300

    add cards

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/animate.min.css';
+import ImageTahiana from '../images/Tahiana.png';
+import ImageTsanta from '../images/Tsanta.png';
+import ImageCedric from '../images/Cedric.png';
 
-function Default() {
+function Default({auteur, updateAuteur}) {
     return (
-        <div>
-            <p>Hello</p>
+        <div className='container'>
+            <br/>
+            <div className='row'>
+                <h1 style={{textAlign:"center"}}>Bienvenue dans notre site web 😉</h1>
+            </div>
+            <br/>
+            <div className='row' style={{textAlign:"center"}}>
+                <div className='col-4 animate__animated animate__slideInLeft'>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={ImageTahiana} alt="Image" height="350px"/>
+                        <div className="card-body">
+                            <h5 className="card-title">ANDRIAMBAHOAKA Tahiana</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button className='btn btn-primary' onClick={() => updateAuteur('Tahiana')}>En savoir plus</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-4 animate__animated animate__slideInUp'>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={ImageTsanta} alt="Image" height="350px"/>
+                        <div className="card-body">
+                            <h5 className="card-title">RAKOTONJANAHARY Tsantaniaina</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button className='btn btn-primary' onClick={() => updateAuteur('Tsanta')}>En savoir plus</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-4 animate__animated animate__slideInRight'>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={ImageCedric} alt="Image" height="350px"/>
+                        <div className="card-body">
+                            <h5 className="card-title">RABARIJOHN <br/> Cedric</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <button className='btn btn-primary' onClick={() => updateAuteur('Cedric')}>En savoir plus</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -11,7 +51,7 @@ function Default() {
 function Tahiana() {
     return (
         <div>
-            <p>Tahiana</p>
+            <p>Page de Tahiana</p>
         </div>
     )
 }
@@ -19,7 +59,7 @@ function Tahiana() {
 function Tsanta() {
     return (
         <div>
-            <p>Tsanta</p>
+            <p>Page de Tsanta</p>
         </div>
     )
 }
@@ -27,7 +67,7 @@ function Tsanta() {
 function Cedric() {
     return (
         <div>
-            <p>Cedric</p>
+            <p>Page de Cedric</p>
         </div>
     )
 }
@@ -40,7 +80,7 @@ function Content({auteur, updateAuteur}) {
     } else if (auteur == 'Cedric') {
         return (<Cedric/>)
     } else {
-        return (<Default/>)
+        return (<Default auteur={auteur} updateAuteur={updateAuteur}/>)
     }
 }
 
