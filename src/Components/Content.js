464ea4f83c8c123ba8ca77b51174f5cc3a4ac647
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-17 18:27:13 +0300

    Update Content.js

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -4,6 +4,43 @@ import ImageTahiana from '../images/Tahiana.png';
 import ImageTsanta from '../images/Tsanta.png';
 import ImageCedric from '../images/Cedric.png';
 
+function Cards({auteur, updateAuteur}) {
+    return (
+        <div className='row' style={{textAlign:"center"}}>
+            <div className='col-4 animate__animated animate__slideInLeft'>
+                <div className="card" style={{width: "75%", marginLeft:"12%"}}>
+                    <img className="card-img-top" src={ImageTahiana} alt="Image" height="350px"/>
+                    <div className="card-body">
+                        <h5 className="card-title">ANDRIAMBAHOAKA <br/> Tahiana</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button className='btn btn-primary' onClick={() => updateAuteur('Tahiana')}>En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+            <div className='col-4 animate__animated animate__slideInUp'>
+                <div className="card" style={{width: "75%", marginLeft:"12%"}}>
+                    <img className="card-img-top" src={ImageTsanta} alt="Image" height="350px"/>
+                    <div className="card-body">
+                        <h5 className="card-title">RAKOTONJANAHARY <br/> Tsantaniaina</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button className='btn btn-primary' onClick={() => updateAuteur('Tsanta')}>En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+            <div className='col-4 animate__animated animate__slideInRight'>
+                <div className="card" style={{width: "75%", marginLeft:"12%"}}>
+                    <img className="card-img-top" src={ImageCedric} alt="Image" height="350px"/>
+                    <div className="card-body">
+                        <h5 className="card-title">RABARIJOHN <br/> Cedric</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button className='btn btn-primary' onClick={() => updateAuteur('Cedric')}>En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function Default({auteur, updateAuteur}) {
     return (
         <div className='container'>
@@ -12,38 +49,7 @@ function Default({auteur, updateAuteur}) {
                 <h1 style={{textAlign:"center"}}>Bienvenue dans notre site web 😉</h1>
             </div>
             <br/>
-            <div className='row' style={{textAlign:"center"}}>
-                <div className='col-4 animate__animated animate__slideInLeft'>
-                    <div className="card" style={{width: "75%", marginLeft:"12%"}}>
-                        <img className="card-img-top" src={ImageTahiana} alt="Image" height="350px"/>
-                        <div className="card-body">
-                            <h5 className="card-title">ANDRIAMBAHOAKA Tahiana</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button className='btn btn-primary' onClick={() => updateAuteur('Tahiana')}>En savoir plus</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-4 animate__animated animate__slideInUp'>
-                    <div className="card" style={{width: "75%", marginLeft:"12%"}}>
-                        <img className="card-img-top" src={ImageTsanta} alt="Image" height="350px"/>
-                        <div className="card-body">
-                            <h5 className="card-title">RAKOTONJANAHARY Tsantaniaina</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button className='btn btn-primary' onClick={() => updateAuteur('Tsanta')}>En savoir plus</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-4 animate__animated animate__slideInRight'>
-                    <div className="card" style={{width: "75%", marginLeft:"12%"}}>
-                        <img className="card-img-top" src={ImageCedric} alt="Image" height="350px"/>
-                        <div className="card-body">
-                            <h5 className="card-title">RABARIJOHN <br/> Cedric</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <button className='btn btn-primary' onClick={() => updateAuteur('Cedric')}>En savoir plus</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Cards auteur={auteur} updateAuteur={updateAuteur}/>
         </div>
     )
 }
