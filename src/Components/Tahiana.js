4be056c830b15db5fb8c47982792fecedcabc6d2
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-03-31 11:10:30 +0300

    fix

--- src/Components/Tahiana.js
+++ src/Components/Tahiana.js
@@ -1,9 +1,35 @@
-function Tahiana() {
+import $ from 'jquery'; 
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+function CallWebService({url, list, updateList}) {
+    React.useEffect(() => {
+        axios
+            .get(url)
+            .then(function (response) {
+                updateList(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
     return (
+        <p>Hello</p>
+    )
+}
+
+function Tahiana() {
+    const [list, updateList] = useState(null)
+    // const url = 'getPanneauxSolaires'
+    return (
+        // <UsersList users={this.state.users}/>
         <div>
-            <p>Page de Tahiana...</p>
+            {/* <CallWebService url={url} list={list} updateList={updateList}/> */}
+            <p>Coucou</p>
         </div>
     )
+    // console.log(list);
 }
 
 export default Tahiana
